c599e95d21e68b24f525246418dc379004f833de
Johannes Weiß <github@tux4u.de>
2012-06-19 18:21:50 +0200

    ExprToMRM: generate a lot of LBS programs

--- ExprToMRM/ExprToMRM.hs
+++ ExprToMRM/ExprToMRM.hs
@@ -2,11 +2,12 @@ module Main (main) where
 
 import Codec.LBS ( Expr(..), InputValues, lbsFromExpr, renderLBSProgram, runLBS
                  , LBSProgram, LBSStmt(..), Register(..), OffsetDirection(..)
-                 , ScaleFactor(..)
+                 , ScaleFactor(..), lbsProgramLength
                  )
 import Data.Monoid (mappend, mconcat)
 import Data.Text.Lazy (Text())
 import Data.Text.Lazy.Builder (Builder(), fromString, toLazyText)
+import System.IO (withFile, IOMode(..))
 import qualified Data.DList as DL
 import qualified Data.Map as M
 import qualified Data.Text.Lazy.IO as TIO
@@ -23,16 +24,29 @@ _Y_ = Var "y"
 --test :: Expr
 --test = (4 * _X_ * _X_ + 2 * (_X_ + _Y_ * _Y_) * _X_ * _Y_ + 7) * _X_
 
-big_sum :: Expr
-big_sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + _X_ + _Y_
-
-test :: Expr
-test = big_sum * big_sum * big_sum * big_sum * big_sum +
-       big_sum * big_sum * big_sum * big_sum * big_sum +
-       big_sum * big_sum * big_sum * big_sum * big_sum
-
+--big_sum :: Expr
+--big_sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + _X_ + _Y_
+--
+--test :: Expr
+--test = big_sum * big_sum * big_sum * big_sum * big_sum +
+--       big_sum * big_sum * big_sum * big_sum * big_sum +
+--       big_sum * big_sum * big_sum * big_sum * big_sum
+--
 --test :: Expr
 --test = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19
+
+--test :: Expr
+--test = 3 * _X_  +  4 * _Y_
+
+--pow :: Num a => a -> Integer -> a
+--pow a p = a ^ p
+
+t :: Expr
+t = ((1 * 2) * (3 * 4)) * ((5 * 6) * (7 * 8)) *
+    ((1 * 2) * (3 * 4)) * ((5 * 6) * (7 * 8))
+
+test :: Expr -- size 255
+test = ((t * t) * (t * t)) * ((t * t) * (t * t))
 
 fromShow :: Show a => a -> Builder
 fromShow = fromString . show
@@ -86,14 +100,32 @@ maximaMatrixFromLBSProgram lbs =
                     (fromString "\n") `mappend`
                     s
 
+doLBS :: Expr -> IO ()
+doLBS e = do
+    putStrLn "Expression:"
+    print e
+    putStrLn "LBSStmt:"
+    TIO.putStrLn $ renderLBSProgram $ lbsFromExpr e
+    putStrLn "EXEC:"
+    print $ runLBS _VARS_ (lbsFromExpr e)
+    putStrLn "LBSStmt:"
+    TIO.putStrLn $ maximaMatrixFromLBSProgram $ lbsFromExpr e
+
 main :: IO ()
 main = do
-    putStrLn "Expression:"
-    print test
-    putStrLn "LBSStmt:"
-    TIO.putStrLn $ renderLBSProgram $ lbsFromExpr test
-    putStrLn "EXEC:"
-    print $ runLBS _VARS_ (lbsFromExpr test)
-    putStrLn "LBSStmt:"
-    TIO.putStrLn $ maximaMatrixFromLBSProgram $ lbsFromExpr test
-    print test
+    doLBS 1
+    print $ lbsProgramLength $ lbsFromExpr test
+    loop 1 14 1
+    where loop :: Int -> Int -> Expr -> IO ()
+          loop i end frag =
+              if i < end
+                then let lbs = lbsFromExpr e'
+                         e' = frag * frag
+                     in do
+                         putStr $ show i ++ ", "
+                         print $ lbsProgramLength lbs
+                         print $ runLBS _VARS_ lbs
+                         --withFile "/dev/null" WriteMode (\h -> TIO.hPutStr h (renderLBSProgram lbs))
+                         loop (i+1) end e'
+                else do
+                    return ()
