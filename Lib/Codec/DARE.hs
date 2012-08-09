c4f006662aa540a6af8c2677affa754411dcfbe9
Johannes Weiß <github@tux4u.de>
2012-08-09 15:53:59 +0200

    ExprToDARE: Switched to universal encoding

--- Lib/Codec/DARE.hs
+++ Lib/Codec/DARE.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Codec.DARE (test
                   , LinearExpr(..)
+                  , DARE(..)
                   ) where
 
 
+import Data.List (foldl')
 import Data.Map (Map)
-import Data.Maybe (mapMaybe)
 import Control.Monad (liftM, liftM2)
 import Control.Monad.CryptoRandom (CRand, runCRand, getCRandom)
 import Crypto.Random (newGenIO, GenError, SystemRandom, CryptoRandomGen)
@@ -14,7 +15,6 @@ import qualified Data.Map as M
 
 type Element = F97
 type Variable = String
-type Decoder a = VarMapping -> a -> Maybe Element
 type VarMapping = Map Variable Element
 
 data PrimaryExpression = Var Variable
@@ -25,6 +25,10 @@ data LinearExpr = LinearExpr { leSlope     :: Element
                              , leIntercept :: Element
                              }
                 | ConstLinearExpr Element
+
+data DARE = DARE { dareMults :: [(LinearExpr, LinearExpr)]
+                 , dareAdds  :: [LinearExpr]
+                 } deriving Show
 
 instance Show LinearExpr where
     show le =
@@ -39,9 +43,9 @@ genLinearExpr :: Element -> PrimaryExpression -> Element -> LinearExpr
 genLinearExpr a b c =
     case b of
       Var b' -> LinearExpr { leSlope = a
-                               , leVariable = b'
-                               , leIntercept = c
-                               }
+                           , leVariable = b'
+                           , leIntercept = c
+                           }
       Const b' -> ConstLinearExpr (a * b' + c)
 
 calcLinearExpr :: VarMapping -> LinearExpr -> Maybe Element
@@ -52,28 +56,12 @@ calcLinearExpr varMap le =
           do vVal <- M.lookup v varMap
              return $ s * vVal + i
 
-mulDecoder :: Decoder [LinearExpr]
-mulDecoder varMap les =
-    case length les of
-      5 -> case (mapMaybe (calcLinearExpr varMap) les) of
-             [e1, e2, e3, e4, e5] -> Just $ e1 * e3 + e2 + e4 + e5
-             _ -> Nothing
-      _ -> Nothing
-
-addDecoder :: Decoder [LinearExpr]
-addDecoder varMap les =
-    case length les of
-      2 -> case (mapMaybe (calcLinearExpr varMap) les) of
-             [e1, e2] -> Just $ e1 + e2
-             _ -> Nothing
-      _ -> Nothing
-
 -- |DARE for a multiplication getting randoms from generator
 dareEncodeMulRnd :: CryptoRandomGen g
                  => PrimaryExpression
                  -> PrimaryExpression
                  -> PrimaryExpression
-                 -> CRand g GenError [LinearExpr]
+                 -> CRand g GenError DARE
 dareEncodeMulRnd x1 x2 x3 =
     do r1 <- getRandomElement
        r2 <- getRandomElement
@@ -85,7 +73,7 @@ dareEncodeMulRnd x1 x2 x3 =
 dareEncodeAddRnd :: CryptoRandomGen g
                  => PrimaryExpression
                  -> PrimaryExpression
-                 -> CRand g GenError [LinearExpr]
+                 -> CRand g GenError DARE
 dareEncodeAddRnd x1 x2 =
     do r <- getRandomElement
        return $ dareEncodeAdd x1 x2 r
@@ -95,11 +83,11 @@ dareEncodeAddRnd x1 x2 =
 dareEncodeAdd :: PrimaryExpression
               -> PrimaryExpression
               -> Element
-              -> [LinearExpr]
+              -> DARE
 dareEncodeAdd x1 x2 r =
     let le1 = genLinearExpr 1 x1 r
         le2 = genLinearExpr 1 x2 (-r)
-        in [le1, le2]
+        in DARE [] [le1, le2]
 
 -- |DARE for a multiplication (+ addition) f(x1, x2, x3) = x1 * x2 + x3
 -- see How to Garble Arithmetic Circuits, p. 13
@@ -110,14 +98,64 @@ dareEncodeMul :: PrimaryExpression
               -> Element
               -> Element
               -> Element
-              -> [LinearExpr]
+              -> DARE
 dareEncodeMul x1 x2 x3 r1 r2 r3 r4 =
     let le1 = genLinearExpr 1 x1 (-r1)
         le2 = genLinearExpr r2 x1 (-r1*r2 + r3)
         le3 = genLinearExpr 1 x2 (-r2)
         le4 = genLinearExpr r1 x2 r4
         le5 = genLinearExpr 1 x3 (-r3-r4)
-        in [le1, le2, le3, le4, le5]
+        in DARE [(le1, le3)] [le2, le4, le5]
+
+addToLinearExpression :: LinearExpr
+                      -> Element
+                      -> LinearExpr
+addToLinearExpression le e =
+    case le of
+      ConstLinearExpr c -> ConstLinearExpr (c + e)
+      LinearExpr s v i -> LinearExpr s v (i + e)
+
+addToDARE :: DARE
+          -> Element
+          -> DARE
+addToDARE (DARE muls adds) e =
+    case adds of
+      [] -> DARE muls [ConstLinearExpr e]
+      (x:xs) -> DARE muls ((addToLinearExpression x e):xs)
+
+-- |DARE for an addition of two DAREs
+dareEncodeDareAdd :: DARE
+                  -> DARE
+                  -> Element
+                  -> DARE
+dareEncodeDareAdd dl dr r =
+    DARE (dlMuls' ++ drMuls') (dlAdds' ++ drAdds')
+    where DARE dlMuls' dlAdds' = addToDARE dl (-r)
+          DARE drMuls' drAdds' = addToDARE dr r
+
+-- |DARE for a DARE addition getting randoms from generator
+dareEncodeDareAddRnd :: CryptoRandomGen g
+                     => DARE
+                     -> DARE
+                     -> CRand g GenError DARE
+dareEncodeDareAddRnd dl dr =
+    do r <- getRandomElement
+       return (dareEncodeDareAdd dl dr r)
+
+-- |DARE decoder
+dareDecode :: VarMapping -> DARE -> Maybe Element
+dareDecode varMap (DARE muls adds) =
+    case sequence (addValsM ++ mulValsM) of
+      Nothing -> Nothing
+      Just vals -> Just $ foldl' (+) 0 vals
+    where addValsM :: [Maybe Element]
+          addValsM = map (calcLinearExpr varMap) adds
+          mulValsM :: [Maybe Element]
+          mulValsM = map doMul muls
+          doMul :: (LinearExpr, LinearExpr) -> Maybe Element
+          doMul (lel, ler) = liftM2 (*) (calcLinearExpr varMap lel)
+                                        (calcLinearExpr varMap ler)
+
 
 _C_1_ :: PrimaryExpression
 _C_1_ = Const 1
@@ -144,18 +182,17 @@ test :: IO ()
 test =
     do g <- (newGenIO :: IO SystemRandom)
        case runCRand testDARE g of
-         Right ((dec, les), _) ->
-            do print $ les
-               print $ dec _TestVarMap_ les
+         Right (dare, _) ->
+            do print $ dare
+               print $ dareDecode _TestVarMap_ dare
          Left _ -> print "left"
        case runCRand (dareEncodeAddRnd _V_y_ _V_x_) g of
          Right (les, _) ->
             do print $ les
-               print $ addDecoder _TestVarMap_ les
+               print $ dareDecode _TestVarMap_ les
          Left _ -> print "left"
     where testDARE =
               do les1 <- dareEncodeMulRnd _V_x_ _C_23_ _C_42_
                  les2 <- dareEncodeAddRnd _V_x_ _C_23_
-                 return (dec, (les1, les2))
-                 where dec v (l1, l2) =
-                           liftM2 (+) (mulDecoder v l1) (addDecoder v l2)
+                 lesOut <- dareEncodeDareAddRnd les1 les2
+                 return lesOut
